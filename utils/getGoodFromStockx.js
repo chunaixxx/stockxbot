8d12a3cdf79f14fc79df40a797377162778fc03d
Daniil Kirillov <chunaixxx@gmail.com>
2021-12-15 14:12:48 +0300

    backup

--- utils/getGoodFromStockx.js
+++ utils/getGoodFromStockx.js
@@ -1,5 +1,9 @@
 import StockXAPI from 'stockx-api'
-const stockX = new StockXAPI()
+
+const stockX = new StockXAPI({
+    currency: 'USD',
+    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1'
+})
 
 const getGoodFromStockx = async url => {
 	try {
