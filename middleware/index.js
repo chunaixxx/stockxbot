9022352efed7b4f3806dfdef2bcc6dac08f9ba1a
Daniil Kirillov <chunaixxx@gmail.com>
2022-02-10 22:39:20 +0300

    fix skip chat

--- middleware/index.js
+++ middleware/index.js
@@ -1,6 +1,6 @@
 import skipBotMessage from './skipBotMessage'
 import checkOnlySub from './checkOnlySub'
-import skipChat from './skipBotMessage'
+import skipChat from './skipChat'
 import checkUser from './checkUser'
 
 export { skipBotMessage, skipChat, checkOnlySub, checkUser }
