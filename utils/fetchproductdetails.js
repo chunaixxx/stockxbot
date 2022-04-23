848ceae18a455960ebf81fabe73bca5515451add
Daniil Kirillov <chunaixxx@gmail.com>
2022-04-23 18:31:19 +0300

    fix product details

--- utils/fetchproductdetails.js
+++ utils/fetchproductdetails.js
@@ -20,16 +20,20 @@ export default async (product, options) => {
     const reqOptions = {
         url: `https://stockx.com/api/products/${webURL}?includes=market&currency=USD&country=US`,
         headers: {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+            'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
             "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             "accept-language": "en-US,en;q=0.9",
-            "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"",
+            "sec-ch-ua": " Not A;Brand\";v=\"99\", \"Chromium\";v=\"100\", \"Google Chrome\";v=\"100",
             "sec-ch-ua-mobile": "?0",
             "sec-fetch-dest": "document",
             "sec-fetch-mode": "navigate",
             "sec-fetch-site": "none",
             "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1"
+            "upgrade-insecure-requests": "1",
+            "cache-control": "max-age=0",
+            "sec-ch-ua-platform": "Windows",
+            
+            
         },
         followAllRedirects: true,
         followRedirect: true,
